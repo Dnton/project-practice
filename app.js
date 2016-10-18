0e6e65ce1172dee672fc4b9963be4093969a28f9
MacBook Pro <MacBookPro@MacBook-Pros-MacBook-Pro.local>
2016-10-18 16:34:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+var layout = require('express-ejs-layouts')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/project-practice')
+
+app.set('view engine', 'ejs')
+app.use(layout)
+app.use(bodyParser.json()) // to parse ajax json requests
+app.use(bodyParser.urlencoded({ extended: true })) // to parse form requests
+
+// serve static files
+app.use(express.static(__dirname + '/public'))
+
+var usersRoute = require('./routes/users')
+var apiUsersRoute = require('./routes/users_api')
+
+app.use('/users', usersRoute) // only render ejs files
+app.use('/api/users', apiUsersRoute) // only handle ajax request
+
+app.listen(3000)
+console.log('Server started')
